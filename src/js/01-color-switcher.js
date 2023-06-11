09b20dfd5bd9a9fc1d5027eba4d875d43f1fa7a0
Denys <bakhonin1@gmail.com>
2023-06-11 19:47:48 +0200

    home-work 9 (task 1)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+console.log(btnStart);
+console.log(btnStop);
+
+intervalValue = null;
+
+btnStart.addEventListener('click', () => {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+  intervalValue = setInterval(() => {
+        btnStart.parentNode.style.backgroundColor = getRandomHexColor();
+    },1000);
+})
+
+btnStop.addEventListener('click', () => {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(intervalValue);
+})
