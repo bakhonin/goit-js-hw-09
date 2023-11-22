f9f40702d09b931763819d7750bb4352bc11b020
bakhonin <122104865+bakhonin@users.noreply.github.com>
2023-11-22 14:08:03 +0000

    Deploying to gh-pages from @ bakhonin/goit-js-hw-09@8a5bb5d7af1e88aadde49e34d4dcf5f95339caa6 🚀

--- 01-color-switcher.9874f6e5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let a=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{e.parentNode.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(a)}));
-//# sourceMappingURL=01-color-switcher.9874f6e5.js.map
